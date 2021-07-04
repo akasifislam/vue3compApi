fa9d3b47f0f9dbcd5f96bf38baf1a69085c67465
akasifislam <aseasifislam@gmail.com>
2021-07-05 01:23:20 +0600

    reactive

--- mojo/src/views/Home.vue
+++ mojo/src/views/Home.vue
@@ -1,36 +1,64 @@
 <template>
   <div class="home"></div>
 
-  <h1>This name is {{ name }} and age is {{ age }}</h1>
-  <button @click="handleClick">click me</button>
+  <h2>Refs</h2>
+  <h1>{{ mastermanOne.name }} - {{ mastermanOne.age }}</h1>
+  <button @click="updateAsifOne">Update</button>
+  <h2>Reactive</h2>
+  <h1>
+    name: {{ mastermanTwo.name }} and age {{ mastermanTwo.age }} -
+    {{ masterTwo }}
+  </h1>
+  <button @click="updateAsifTwo">Update</button>
+  <!-- <button @click="handleClick">click me</button>
   <input type="text" v-model="name" />
 
-  <button @click.prevent="age++">Age</button>
+  <button @click.prevent="age++">Age</button> -->
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
+import { reactive, ref } from "@vue/reactivity";
 export default {
   name: "Home",
 
   setup() {
-    console.log(this);
+    const mastermanOne = ref({ name: "muhammad asif ul islam", age: 26 });
+    const mastermanTwo = reactive({ name: "ema watson", age: 25 });
 
-    // const p = ref("ami ref er custom data");
-    // console.log(p, p.value);
-    const name = ref("mastrer man");
-    const age = ref(33);
+    const masterOne = ref("master One");
+    const masterTwo = ref("master Two");
 
-    const handleClick = () => {
-      name.value = "ami ki ar bolbo";
-      age.value = 22;
+    const updateAsifOne = () => {
+      mastermanOne.value.name = "zjskhfkjs rsfguiwer";
+      mastermanOne.value.age = 40;
+    };
+    const updateAsifTwo = () => {
+      mastermanTwo.name = "dfzgdfg";
     };
 
     return {
-      name,
-      age,
-      handleClick,
+      mastermanOne,
+      updateAsifOne,
+      mastermanTwo,
+      updateAsifTwo,
+      masterOne,
+      masterTwo,
     };
+
+    // ============================================================
+
+    // console.log(this);
+
+    // const p = ref("ami ref er custom data");
+    // console.log(p, p.value);
+    // const name = ref("mastrer man");
+    // const age = ref(33);
+
+    // return {
+    //   name,
+    //   age,
+    //   handleClick,
+    // };
   },
 };
 </script>
