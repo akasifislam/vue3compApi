467dc7d9d769f64240c1c751bbe797dd76177ce8
akasifislam <aseasifislam@gmail.com>
2021-07-04 22:41:41 +0600

    template refs

--- mojo/src/views/Home.vue
+++ mojo/src/views/Home.vue
@@ -1,26 +1,34 @@
 <template>
   <div class="home"></div>
 
-  <h1>My name is {{ name }} and age is {{ age }}</h1>
+  <h1 ref="p">This name is {{ name }} and age is {{ age }}</h1>
   <button @click="handleClick">click me</button>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 export default {
   name: "Home",
 
   setup() {
+    console.log(this);
+
+    const p = ref("ami ref er custom data");
+    console.log(p, p.value);
     let name = "mastrer man";
     let age = "26";
 
     const handleClick = () => {
-      console.log("you click me");
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "Amar kono kaj nai nak";
     };
 
     return {
       name,
       age,
       handleClick,
+      p,
     };
   },
 };
