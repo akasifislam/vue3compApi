ae12325abe1d540ba771f77361e868a12a0c1a62
akasifislam <aseasifislam@gmail.com>
2021-07-06 22:18:58 +0600

    computed values

--- mojo/src/views/Home.vue
+++ mojo/src/views/Home.vue
@@ -1,64 +1,33 @@
 <template>
-  <div class="home"></div>
-
-  <h2>Refs</h2>
-  <h1>{{ mastermanOne.name }} - {{ mastermanOne.age }}</h1>
-  <button @click="updateAsifOne">Update</button>
-  <h2>Reactive</h2>
-  <h1>
-    name: {{ mastermanTwo.name }} and age {{ mastermanTwo.age }} -
-    {{ masterTwo }}
-  </h1>
-  <button @click="updateAsifTwo">Update</button>
-  <!-- <button @click="handleClick">click me</button>
-  <input type="text" v-model="name" />
-
-  <button @click.prevent="age++">Age</button> -->
+  <div class="home">
+    <h1>Home</h1>
+      <input type="text" v-model="search">
+      <p>search term - {{ search }} </p>
+      <ul v-for="name in machingNames" :key="name.id">
+        <li>{{ name }} </li> 
+      </ul>
+  </div>
 </template>
 
 <script>
-import { reactive, ref } from "@vue/reactivity";
+import { computed, ref } from "@vue/reactivity";
 export default {
   name: "Home",
 
   setup() {
-    const mastermanOne = ref({ name: "muhammad asif ul islam", age: 26 });
-    const mastermanTwo = reactive({ name: "ema watson", age: 25 });
+    const search = ref("")
+    const names = ref(['morgan','massi','trump','azam','malik','ronaldo']);
 
-    const masterOne = ref("master One");
-    const masterTwo = ref("master Two");
+    const machingNames = computed(() =>{
+      return names.value.filter((name) => name.includes(search.value))
+    })
 
-    const updateAsifOne = () => {
-      mastermanOne.value.name = "zjskhfkjs rsfguiwer";
-      mastermanOne.value.age = 40;
-    };
-    const updateAsifTwo = () => {
-      mastermanTwo.name = "dfzgdfg";
-    };
 
     return {
-      mastermanOne,
-      updateAsifOne,
-      mastermanTwo,
-      updateAsifTwo,
-      masterOne,
-      masterTwo,
+      names,
+      search,
+      machingNames
     };
-
-    // ============================================================
-
-    // console.log(this);
-
-    // const p = ref("ami ref er custom data");
-    // console.log(p, p.value);
-    // const name = ref("mastrer man");
-    // const age = ref(33);
-
-    // return {
-    //   name,
-    //   age,
-    //   handleClick,
-    // };
   },
 };
 </script>
---
+++ mojo/src/views/Home2.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="home"></div>
+
+  <h2>Refs</h2>
+  <h1>{{ mastermanOne.name }} - {{ mastermanOne.age }}</h1>
+  <button @click="updateAsifOne">Update</button>
+  <h2>Reactive</h2>
+  <h1>
+    name: {{ mastermanTwo.name }} and age {{ mastermanTwo.age }} -
+    {{ masterTwo }}
+  </h1>
+  <button @click="updateAsifTwo">Update</button>
+  <!-- <button @click="handleClick">click me</button>
+  <input type="text" v-model="name" />
+
+  <button @click.prevent="age++">Age</button> -->
+</template>
+
+<script>
+import { reactive, ref } from "@vue/reactivity";
+export default {
+  name: "Home",
+
+  setup() {
+    const mastermanOne = ref({ name: "muhammad asif ul islam", age: 26 });
+    const mastermanTwo = reactive({ name: "ema watson", age: 25 });
+
+    const masterOne = ref("master One");
+    const masterTwo = ref("master Two");
+
+    const updateAsifOne = () => {
+      mastermanOne.value.name = "zjskhfkjs rsfguiwer";
+      mastermanOne.value.age = 40;
+    };
+    const updateAsifTwo = () => {
+      mastermanTwo.name = "dfzgdfg";
+    };
+
+    return {
+      mastermanOne,
+      updateAsifOne,
+      mastermanTwo,
+      updateAsifTwo,
+      masterOne,
+      masterTwo,
+    };
+
+    // ============================================================
+
+    // console.log(this);
+
+    // const p = ref("ami ref er custom data");
+    // console.log(p, p.value);
+    // const name = ref("mastrer man");
+    // const age = ref(33);
+
+    // return {
+    //   name,
+    //   age,
+    //   handleClick,
+    // };
+  },
+};
+</script>
